0a250b64e602a6535cfa7d4fa5a82d64e523d247
William <78540233+williamlidberg@users.noreply.github.com>
2022-03-05 21:32:40 +0100

    Update Select_tiles_around_block.py

--- Select_tiles_around_block.py
+++ Select_tiles_around_block.py
@@ -1,9 +1,8 @@
 import os
 import geopandas as gpd
 import pandas as pd
-from geopandas.tools import sjoin
-import glob
 import shutil
+from geopandas.tools import sjoin
 
 # read lidar tile index to geopnadas dataframe
 lidar_tiles_index = gpd.read_file('E:/William/Indexrutor/Indexrutor_2_5km_Sverige.shp')
@@ -19,15 +18,14 @@ block_extent = gpd.GeoDataFrame(pd.concat(json_list, ignore_index=True), crs=jso
 # intersecct lidar tiles with block extent with one tile overlap
 intersect = gpd.sjoin(lidar_tiles_index, block_extent, how='inner', op='intersects')
 # get uniqe names
-uniqe_names = intersect['LasNamn'].unique()
+uniqe_names = intersect['Indexruta'].unique()
 
 
 path_to_downloaded_data = 'E:/William/laserdataskog/pooled/'
 path_to_working_dir = 'E:/William/laserdataskog/workdir/'
-for tile in uniqe_names:
-    downladed_tile = path_to_downloaded_data + tile + '.laz'
-    copied_tile = path_to_working_dir + tile + '.laz'
-    try:
+names_relevant_tiles = []
+for name in os.listdir(path_to_downloaded_data):
+    if name.endswith('.laz') and os.path.basename(name[7:20]) in uniqe_names:
+        downladed_tile = path_to_downloaded_data + name
+        copied_tile = path_to_working_dir + name
         shutil.copy(downladed_tile, copied_tile)
-    except:
-        print('error',copied_tile)
