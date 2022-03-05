384b05d7a922d8f2407a97dbcf838d28288d2f5d
William <78540233+williamlidberg@users.noreply.github.com>
2022-03-05 16:07:07 +0100

    Update Select_tiles_around_block.py

--- Select_tiles_around_block.py
+++ Select_tiles_around_block.py
@@ -2,6 +2,9 @@ import os
 import geopandas as gpd
 import pandas as pd
 from geopandas.tools import sjoin
+import glob
+import shutil
+
 # read lidar tile index to geopnadas dataframe
 lidar_tiles_index = gpd.read_file('E:/William/Indexrutor/Indexrutor_2_5km_Sverige.shp')
 path_to_block_metadata = 'E:/William/laserdataskog/20D001/metadata/'
@@ -16,4 +19,15 @@ block_extent = gpd.GeoDataFrame(pd.concat(json_list, ignore_index=True), crs=jso
 # intersecct lidar tiles with block extent with one tile overlap
 intersect = gpd.sjoin(lidar_tiles_index, block_extent, how='inner', op='intersects')
 # get uniqe names
-len(intersect['LasNamn'].unique())
+uniqe_names = intersect['LasNamn'].unique()
+
+
+path_to_downloaded_data = 'E:/William/laserdataskog/pooled/'
+path_to_working_dir = 'E:/William/laserdataskog/workdir/'
+for tile in uniqe_names:
+    downladed_tile = path_to_downloaded_data + tile + '.laz'
+    copied_tile = path_to_working_dir + tile + '.laz'
+    try:
+        shutil.copy(downladed_tile, copied_tile)
+    except:
+        print('error',copied_tile)
