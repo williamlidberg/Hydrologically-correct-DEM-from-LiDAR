fd3899623534fb64d334f0959853682178ed8eed
williamlidberg <william.lidberg@gmail.com>
2022-12-05 14:32:44 +0100

    Update Master.sh

--- Master.sh
+++ Master.sh
@@ -3,7 +3,7 @@
 #docker run --mount type=bind,source=/mnt/GIS/hydrologically_correct_dem_1m/,target=/data --mount type=bind,source=/mnt/Extension_100TB/national_datasets/,target=/national --mount type=bind,source=/mnt/Extension_100TB/William/GitHub/Hydrologically-correct-DEM-from-LiDAR/,target=/code dem:latest python3 code/create_isobasins.py /temp/ /data/dem50m/dem_50m.tif /data/smhi/havsomraden2008_swe.shp 160000 /data/isobasins/isobasins.shp /data/isobasins/split/
 
 #echo "CLIPP DATA"
-echo "Clipping DEM with Isobasins"
+#echo "Clipping DEM with Isobasins"
 #time docker run --mount type=bind,source=/mnt/GIS/hydrologically_correct_dem_1m/,target=/data --mount type=bind,source=/mnt/Extension_100TB/national_datasets/,target=/national --mount type=bind,source=/mnt/Extension_100TB/William/GitHub/Hydrologically-correct-DEM-from-LiDAR/,target=/code dem:latest python3 code/split_dem_by_isobasins.py /temp/ /national/Swedish1mDEM_old/tiles/ /national/Swedish1mDEM_old/dem1m.vrt /data/isobasins/split/ /data/clipraster/dem/ -32768
 #echo "Reclassifying ditches"
 #time docker run --mount type=bind,source=/mnt/GIS/hydrologically_correct_dem_1m/,target=/data --mount type=bind,source=/mnt/Extension_100TB/national_datasets/,target=/national --mount type=bind,source=/mnt/Extension_100TB/William/GitHub/Hydrologically-correct-DEM-from-LiDAR/,target=/code dem:latest python3 code/reclassify_ditches.py /national/ditches/1m/probability/ /data/reclassified_ditches/
@@ -17,11 +17,10 @@ echo "Clipping DEM with Isobasins"
 #time docker run --mount type=bind,source=/mnt/GIS/hydrologically_correct_dem_1m/,target=/data --mount type=bind,source=/mnt/Extension_100TB/national_datasets/,target=/national --mount type=bind,source=/mnt/Extension_100TB/William/GitHub/Hydrologically-correct-DEM-from-LiDAR/,target=/code dem:latest python3 code/split_geopackage_by_isobasins.py /data/isobasins/split/ /data/culverts_line/ /data/clipvector/culverts_line/
 
 
-#echo "PRE-PROCESS DEM"
-#time docker run --mount type=bind,source=/mnt/GIS/hydrologically_correct_dem_1m/,target=/data --mount type=bind,source=/mnt/Extension_100TB/William/GitHub/Hydrologically-correct-DEM-from-LiDAR/,target=/code dem:latest python3 code/preprocess.py /temp/ /data/clipraster/dem/ /data/clipraster/ditches/ /data/clipvector/streams/ /data/clipvector/roads_rail/ /data/clipvector/culverts_line/ /data/preprocessed/
-#echo "Done"
-time docker run --mount type=bind,source=/mnt/GIS/hydrologically_correct_dem_1m/,target=/data --mount type=bind,source=/mnt/Extension_100TB/William/GitHub/Hydrologically-correct-DEM-from-LiDAR/,target=/code dem:latest python3 code/preprocess.py /temp/ /data/clipraster/dem_sizetest/ /data/clipraster/ditches/ /data/clipvector/streams/ /data/clipvector/roads_rail/ /data/clipvector/culverts_line/ /data/preprocessed/
+echo "PRE-PROCESS DEM"
+time docker run --mount type=bind,source=/mnt/GIS/hydrologically_correct_dem_1m/,target=/data --mount type=bind,source=/mnt/Extension_100TB/William/GitHub/Hydrologically-correct-DEM-from-LiDAR/,target=/code dem:latest python3 code/preprocess.py /temp/ /data/clipraster/dem/ /data/clipraster/ditches/ /data/clipvector/streams/ /data/clipvector/roads_rail/ /data/clipvector/culverts_line/ /data/preprocessed/
 echo "Done"
+
 exit
 
 
